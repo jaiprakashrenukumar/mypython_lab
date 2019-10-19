12a48057952346553b04c55372a95f0789a87e96
Jaiprakashrenukumar <rjai.jp19@gmail.com>
2019-10-19 18:23:17 +0530

    functions in turtle

---
+++ turtle/08_functions_turtle.py
@@ -0,0 +1,17 @@
+import turtle
+bob = turtle.Turtle()
+def square():
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(100)
+    bob.left(90)
+    bob.forward(100)
+    bob.left(90)
+
+square()
+
+bob.forward(100)
+
+square()

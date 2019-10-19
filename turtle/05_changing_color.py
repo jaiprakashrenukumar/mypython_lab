7d5060a4884cfad169d843eb90359e1f4ec3f6de
Jaiprakashrenukumar <rjai.jp19@gmail.com>
2019-10-19 17:24:03 +0530

    changing color

---
+++ turtle/05_changing_color.py
@@ -0,0 +1,11 @@
+import turtle
+color = turtle.Turtle()
+color.pencolor("blue")
+for i in range(50):
+    color.forward(50)
+    color.left(123)
+color.pencolor("red")
+for i in range(50):
+    color.forward(100)
+    color.left(123) 
+turtle.done()

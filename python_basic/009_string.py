68f160eec607e58b0062bf43ccb4dd149b573ee2
Jaiprakashrenukumar <rjai.jp19@gmail.com>
2019-12-28 12:48:31 +0530

    string in python

---
+++ python_basic/009_string.py
@@ -0,0 +1,26 @@
+# https://stackoverflow.com/questions/509211/understanding-slice-notation
+
+
+name = "Jaiprakash"
+print(name)
+print("\n")
+
+#print first letter  of the name
+print("print first letter  of the name ")
+print(name[0])
+print("\n")
+
+# print last letter from the name
+print("print last letter from the name ")
+print(name[9])
+print("\n")
+
+# remove first three character from the name
+print("remove first three character from the name") 
+print(name[3:])
+print("\n")
+
+# print only first 3 characters
+print("print only first 3 characters")
+print(name[:3])
+print("\n")

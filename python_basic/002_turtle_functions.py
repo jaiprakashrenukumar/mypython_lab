142764961a93e731a85bee96131c5f0f545389da
Jaiprakashrenukumar <rjai.jp19@gmail.com>
2019-12-28 10:59:09 +0530

    functions in python

---
+++ python_basic/002_turtle_functions.py
@@ -0,0 +1,19 @@
+import turtle
+import time
+
+jai_turtle = turtle.Turtle()
+def square():    
+    jai_turtle.forward(100)
+    jai_turtle.right(90)
+    jai_turtle.forward(100)
+    jai_turtle.right(90)
+    jai_turtle.forward(100)
+    jai_turtle.right(90)
+    jai_turtle.forward(100)
+    
+square()
+
+jai_turtle.forward(100)
+
+square()
+time.sleep(3)

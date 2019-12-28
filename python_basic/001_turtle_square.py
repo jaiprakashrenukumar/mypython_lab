2c694904c5243b3d62492b44013c6ffdcae2332b
Jaiprakashrenukumar <rjai.jp19@gmail.com>
2019-12-28 10:49:16 +0530

    turtle square

---
+++ python_basic/001_turtle_square.py
@@ -0,0 +1,11 @@
+import turtle
+import time
+jai = turtle.Turtle()
+jai.forward(100)
+jai.right(90)
+jai.forward(100)
+jai.right(90)
+jai.forward(100)
+jai.right(90)
+jai.forward(100)
+time.sleep(5)
